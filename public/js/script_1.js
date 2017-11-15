68be2143452aeae21c2b4d287e471a90b27a9f78
jverbosky <jverbosk@gmail.com>
2017-11-15 10:22:27 -0500

    basic primer ready

---
+++ public/js/script_1.js
@@ -0,0 +1,9 @@
+function helloWorld() {
+
+  console.log("helloWorld() called...");
+
+  // var targetDiv = document.getElementById("target");
+
+  // targetDiv.innerHTML = "Hello World!";
+
+}
---
+++ public/js/script_2.js
@@ -0,0 +1,20 @@
+function helloWorld() {
+
+  console.log("helloWorld() called...");
+
+  // var targetDiv = document.getElementById("target");
+
+  // targetDiv.innerHTML = "Hello World!";
+
+}
+
+
+function appendElement() {
+
+  console.log("appendElement() called...");
+
+  // var targetDiv = document.getElementById("target");
+  // var paragraphElement = "and more text..."
+  
+  // targetDiv.append(paragraphElement);  
+}
